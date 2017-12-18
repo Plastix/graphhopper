d6e8876de4801989f79dec3e646470396c6b2c79
Plastix <aidanpieper@gmail.com>
2017-12-18 13:40:12 -0500

    Misc refactoring

--- core/src/main/java/com/graphhopper/routing/ils/ShortestPathCalculator.java
+++ core/src/main/java/com/graphhopper/routing/ils/ShortestPathCalculator.java
@@ -1,8 +1,23 @@
 package com.graphhopper.routing.ils;
 
 import com.graphhopper.routing.Path;
+import com.sun.istack.internal.NotNull;
 
 public interface ShortestPathCalculator {
 
+    /**
+     * Returns the shortest distance in meters between two nodes of the graph.
+     */
     Path shortestPath(int s, int d);
+
+    /**
+     * Returns the total distance in meters of the path s --> arc --> d where "-->" is shortest path.
+     *
+     * @param s   Start node ID
+     * @param d   End node ID
+     * @param arc Arc
+     * @return Distance in meters
+     */
+    double getPathCost(int s, int d, @NotNull Arc arc);
+
 }
