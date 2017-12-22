5b9b57f2d1eeed80f8e7fef88a9d0b6c0d9ab4ec
Plastix <aidanpieper@gmail.com>
2017-12-22 09:22:14 -0500

    Update getAllArcs to only return arcs inside of Ellipse

--- core/src/main/java/com/graphhopper/routing/ils/Ellipse.java
+++ core/src/main/java/com/graphhopper/routing/ils/Ellipse.java
@@ -2,9 +2,11 @@ package com.graphhopper.routing.ils;
 
 import com.graphhopper.util.DistanceCalc;
 import com.graphhopper.util.Helper;
+import com.graphhopper.util.shapes.BBox;
 import com.graphhopper.util.shapes.GHPoint;
+import com.graphhopper.util.shapes.Shape;
 
-public class Ellipse {
+public class Ellipse implements Shape {
 
     private DistanceCalc calc = Helper.DIST_EARTH;
     private GHPoint focus1;
@@ -17,8 +19,51 @@ public class Ellipse {
         this.radius = radius;
     }
 
+    @Override
+    public boolean intersect(Shape o) {
+        return false;
+    }
+
+    @Override
     public boolean contains(double lat, double lon) {
-        return calc.calcNormalizedDist(lat, lon, focus1.lat, focus1.lon) +
-                calc.calcNormalizedDist(lat, lon, focus2.lat, focus2.lon) <= radius;
+        return calc.calcDist(lat, lon, focus1.lat, focus1.lon) +
+                calc.calcDist(lat, lon, focus2.lat, focus2.lon) <= radius;
+    }
+
+    @Override
+    public boolean contains(Shape s) {
+        return false;
+    }
+
+    @Override
+    public BBox getBounds() {
+        return null;
+    }
+
+    @Override
+    public GHPoint getCenter() {
+        GHPoint point = new GHPoint();
+
+        double dLon = Math.toRadians(focus2.lon - focus1.lon);
+
+        //convert to radians
+        double lat1 = Math.toRadians(focus1.lat);
+        double lat2 = Math.toRadians(focus2.lat);
+        double lon1 = Math.toRadians(focus1.lon);
+
+        double Bx = Math.cos(lat2) * Math.cos(dLon);
+        double By = Math.cos(lat2) * Math.sin(dLon);
+        double lat3 = Math.atan2(Math.sin(lat1) + Math.sin(lat2), Math.sqrt((Math.cos(lat1) + Bx) * (Math.cos(lat1) + Bx) + By * By));
+        double lon3 = lon1 + Math.atan2(By, Math.cos(lat1) + Bx);
+
+        point.lat = Math.toDegrees(lat3);
+        point.lon = Math.toDegrees(lon3);
+        return point;
+    }
+
+
+    @Override
+    public double calculateArea() {
+        return 0;
     }
 }
